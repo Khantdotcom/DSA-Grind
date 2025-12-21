7932e4a4384b73af8552a427fbe6b56d2f8ac28e
Khantdotcom <kokhanthtay@gmail.com>
2025-12-21 18:10:24 +0700

    Solved : Advent of code day 3

---
+++ Advent of Code/test.py
@@ -0,0 +1,30 @@
+def findLargestCombo(num_str):
+    index_dict = {}
+    for idx,ch in enumerate(num_str):
+        ch = int(ch)
+        if ch not in index_dict:
+            index_dict[ch] = []
+        index_dict[ch].append(idx)
+    for i in 
+    result = ""
+    while True:
+        biggest_num = None
+        smallest_idx = None
+        for digit in sorted(index_dict.keys(),reverse=True):
+            if not index_dict[digit]:
+                continue
+            candidate_index = index_dict[digit][0]
+            if biggest_num is None:
+                biggest_num = digit
+                smallest_idx = candidate_index
+            else:
+                if digit > biggest_num or candidate_index < smallest_idx:
+                    biggest_num = digit
+                    smallest_idx = candidate_index
+        if biggest_num is None:
+            break
+        result += str(biggest_num)
+        index_dict[biggest_num].pop(0)
+    return result
+    
+print(findLargestCombo("4243411423211141423114142211242211313243142414412343314441331444324234332241124232434314434334456789"))
